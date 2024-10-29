66c75ec48c59fa543223293d38724c9789d57cf6
ketherball <121590283+ketherball@users.noreply.github.com>
2024-10-29 13:16:10 -0400

    feat: implement heap initialization method

--- heapalloc.c
+++ heapalloc.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 #include "heapalloc.h"
 
 #define CAPACITY 640000
@@ -10,6 +14,21 @@ typedef struct {
 char bitmap[CAPACITY] = {  0 * CAPACITY };
 
 static Heapdata heapdata;
+
+static void heapinit() {
+    void* address_space = mmap(NULL, CAPACITY,
+                          PROT_READ|PROT_WRITE,
+                          MAP_PRIVATE|MAP_ANONYMOUS,
+                          0, 0);
+
+    if (address_space == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    heapdata.heap = address_space;
+    heapdata.freedspace = CAPACITY;
+}
 
 void* heapalloc(size_t size) {
 
