9e5ee5055814468ff468d8f27bb4452ed6332450
ketherball <121590283+ketherball@users.noreply.github.com>
2024-10-29 20:07:17 -0400

    tests: implement basic heapalloc tests

--- testsuites/testsuites.c
+++ testsuites/testsuites.c
@@ -1,24 +1,23 @@
 #include "../utils/test/test.h"
+#include "../heapalloc.h"
+#include "../bitmap/bitmap.h"
 
-void heapinit_tests();
 void heapalloc_tests();
 void heapfree_tests();
 
 void heap_testsuites() {
-    suite("HEAP INIT TESTS", heapinit_tests);
     suite("HEAP ALLOC TESTS", heapalloc_tests);
     suite("HEAP FREE TESTS", heapfree_tests);
 }
 
-void heapinit_tests() {
-
-    it("Works!", 1);
-    it("Doesn't work :(", 0);
-}
-
 void heapalloc_tests() {
-    it("Works!", 1);
-    it("Doesn't work :(", 0);
+    char* bitmap = getbitmap();
+
+    heapalloc(sizeof(char) * 2);
+    it("Allocates to appropriate place on bitmap", bitmap[0] != 0 && bitmap[1] != 0);
+    heapalloc(sizeof(int));
+    heapalloc(sizeof(char) * 2);
+    it("Sets value of first bit of allocated to chunk to requested size", bitmap[6] == 2);
 }
 
 void heapfree_tests() {
