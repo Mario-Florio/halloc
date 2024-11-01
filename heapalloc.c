d5e1251af7decdbb7954d455d5d13bee91cd91f1
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-01 15:30:18 -0400

    refactor: implement helper function to throw errors

--- heapalloc.c
+++ heapalloc.c
@@ -16,16 +16,15 @@ char bitmap[CAPACITY] = { 0 * CAPACITY };
 
 static Heapdata heapdata;
 
+static void throwerror(char* name);
+
 static void heapinit() {
     void* address_space = mmap(NULL, CAPACITY,
                                PROT_READ|PROT_WRITE,
                                MAP_PRIVATE|MAP_ANONYMOUS,
                                0, 0);
 
-    if (address_space == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
+    if (address_space == MAP_FAILED) throwerror("mmap");
 
     heapdata.heap = address_space;
     heapdata.freedspace = CAPACITY;
@@ -35,21 +34,12 @@ static int getSmallestFreeSpace(size_t size);
 
 void* heapalloc(size_t size) {
     if (heapdata.heap == NULL) heapinit();
-    if (size > heapdata.freedspace) {
-        perror("Reached max heap capacity");
-        exit(1);
-    }
-    if (size < 1) {
-        perror("Must request size greater than 0");
-        exit(1);
-    }
+    if (size > heapdata.freedspace) throwerror("Reached max heap capacity");
+    if (size < 1) throwerror("Must request size greater than 0");
 
     int start = getSmallestFreeSpace(size);
 
-    if (start == -1) {
-        perror("Unable to fulfill request");
-        exit(1);
-    }
+    if (start == -1) throwerror("Unable to fulfill request");
 
     for (int i = start; i < start+size; i++) {
         bitmap[i] = 1;
@@ -76,15 +66,17 @@ void heapfree(void* ptr) {
         }
     }
 
-    if (chunksize == 0) {
-        perror("Memory address is already free");
-        exit(1);
-    }
+    if (chunksize == 0) throwerror("Memory address is already free");
     
     heapdata.freedspace += chunksize;
 }
 
 // UTILS
+static void throwerror(char* name) {
+    perror(name);
+    exit(1);
+}
+
 static int getSmallestFreeSpace(size_t size) {
     int currIdx = 0, currSpace = 0, found = 0;
     int smallestIdx = 0, smallestSpace = CAPACITY+1;
