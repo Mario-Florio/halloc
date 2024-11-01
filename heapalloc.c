eb8dbc763513ce4d52040247bc0739a86b9536af
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-01 15:51:46 -0400

    style: update naming

--- heapalloc.c
+++ heapalloc.c
@@ -6,17 +6,17 @@
 #include "heapalloc.h"
 
 #define CAPACITY 640000
-
-typedef struct {
-    void* heap;
-    int freedspace;
-} Heapdata;
-
 char bitmap[CAPACITY] = { 0 * CAPACITY };
 
-static Heapdata heapdata;
+typedef struct {
+    void* start;
+    int free_space;
+} Heap;
+
+static Heap heap;
 
 static void throwerror(char* name);
+static int getsmallestfreespace(size_t size);
 
 static void heapinit() {
     void* address_space = mmap(NULL, CAPACITY,
@@ -26,19 +26,17 @@ static void heapinit() {
 
     if (address_space == MAP_FAILED) throwerror("mmap");
 
-    heapdata.heap = address_space;
-    heapdata.freedspace = CAPACITY;
+    heap.start = address_space;
+    heap.free_space = CAPACITY;
 }
-
-static int getSmallestFreeSpace(size_t size);
 
 void* heapalloc(size_t size) {
     if (size > 127) throwerror("Sizes greater than 127 bits not supported");
-    if (heapdata.heap == NULL) heapinit();
-    if (size > heapdata.freedspace) throwerror("Reached max heap capacity");
+    if (heap.start == NULL) heapinit();
+    if (size > heap.free_space) throwerror("Reached max heap capacity");
     if (size < 1) throwerror("Must request size greater than 0");
 
-    int start = getSmallestFreeSpace(size);
+    int start = getsmallestfreespace(size);
 
     if (start == -1) throwerror("Unable to fulfill request");
 
@@ -47,19 +45,19 @@ void* heapalloc(size_t size) {
     }
     bitmap[start] = size;
 
-    void* memaddress = (void*)heapdata.heap+start;
-    heapdata.freedspace -= size;
+    void* mem_address = (void*)heap.start+start;
+    heap.free_space -= size;
 
-    return memaddress;
+    return mem_address;
 }
 
 void heapfree(void* ptr) {
-    int chunksize = 0;
+    int chunk_size = 0;
     for (int i = 0; i < CAPACITY; i++) {
-        if (heapdata.heap+i == ptr) {
-            chunksize = bitmap[i];
+        if (heap.start+i == ptr) {
+            chunk_size = bitmap[i];
             int j = i;
-            while (j < chunksize+i) {
+            while (j < chunk_size+i) {
                 bitmap[j] = 0;
                 j++;
             }
@@ -67,9 +65,9 @@ void heapfree(void* ptr) {
         }
     }
 
-    if (chunksize == 0) throwerror("Memory address is already free");
+    if (chunk_size == 0) throwerror("Memory address is already free");
     
-    heapdata.freedspace += chunksize;
+    heap.free_space += chunk_size;
 }
 
 // UTILS
@@ -78,30 +76,30 @@ static void throwerror(char* name) {
     exit(1);
 }
 
-static int getSmallestFreeSpace(size_t size) {
-    int currIdx = 0, currSpace = 0, found = 0;
-    int smallestIdx = 0, smallestSpace = CAPACITY+1;
+static int getsmallestfreespace(size_t size) {
+    int curr_idx = 0, curr_space = 0, found = 0;
+    int smallest_idx = 0, smallest_space = CAPACITY+1;
 
     for (int i = 0; i < CAPACITY+1; i++) {
-        if (currSpace == size) found = 1;
-        if (smallestSpace == size) break;
+        if (curr_space == size) found = 1;
+        if (smallest_space == size) break;
         if (bitmap[i] == 0) {
-            currSpace++;
+            curr_space++;
         } else {
-            if (currSpace >= size && currSpace < smallestSpace) {
-                smallestSpace = currSpace;
-                smallestIdx = currIdx;
+            if (curr_space >= size && curr_space < smallest_space) {
+                smallest_space = curr_space;
+                smallest_idx = curr_idx;
             }
-            currIdx = i+1;
-            currSpace = 0;
+            curr_idx = i+1;
+            curr_space = 0;
         }
     }
 
     // used in cases where "else" block doesn't run
-    if (currSpace >= size && currSpace < smallestSpace) {
-        smallestSpace = currSpace;
-        smallestIdx = currIdx;
+    if (curr_space >= size && curr_space < smallest_space) {
+        smallest_space = curr_space;
+        smallest_idx = curr_idx;
     }
 
-    return found ? smallestIdx : -1;
+    return found ? smallest_idx : -1;
 }
